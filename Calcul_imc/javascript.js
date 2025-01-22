af71ae2c5940ff896ef865b41845d510240a8273
yannick biot <yannickpro64@gmail.com>
2025-01-22 17:32:36 +0100

    controle des inputs

--- Calcul_imc/javascript.js
+++ Calcul_imc/javascript.js
@@ -4,7 +4,21 @@ let form = document.getElementById("formulaire");
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   // On va récupérer et contrôler l'input pour le poid
-  const poids = document.getElementById("poids").value;
-  const taille = document.getElementById("taille").value;
-  console.log(`ma taille ${taille} et mon poids ${poids}`);
+  const poids = document.getElementById("poids");
+  const taille = document.getElementById("taille");
+  // controle des inputs
+  if (
+    poids.value <= 0 ||
+    !Number.isInteger(parseInt(poids.value)) ||
+    taille.value <= 0 ||
+    !Number.isInteger(parseInt(taille.value))
+  ) {
+    alert("Erreur d'entrées");
+    initialisation(poids, taille);
+  }
+  // fonction d'initialisation des paramètres
+  function initialisation(poids, taille) {
+    poids.value = 0;
+    taille.value = 0;
+  }
 });
