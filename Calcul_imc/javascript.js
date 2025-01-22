e698d2c4d1a6ad21e0983a92d69fa405a51dcf59
yannick biot <yannickpro64@gmail.com>
2025-01-22 16:18:07 +0100

    Verification des saisies et recuperation des valeurs

--- Calcul_imc/javascript.js
+++ Calcul_imc/javascript.js
@@ -1,2 +1,10 @@
 // mon javascript
-console.log("test")
+// At first i stop the submit action
+let form = document.getElementById("formulaire");
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  // On va récupérer et contrôler l'input pour le poid
+  const poids = document.getElementById("poids").value;
+  const taille = document.getElementById("taille").value;
+  console.log(`ma taille ${taille} et mon poids ${poids}`);
+});
