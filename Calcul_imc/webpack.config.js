58cf3f57acf8e41341b5f97a519f72f8867896a8
yannick biot <yannickpro64@gmail.com>
2025-01-25 11:02:54 +0100

    configuration de webpack-dev-server

--- Calcul_imc/webpack.config.js
+++ Calcul_imc/webpack.config.js
@@ -8,6 +8,14 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
   },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'dist'), // Dossier pour servir les fichiers
+    },
+    port: 8080, // Port par défaut (http://localhost:8080)
+    open: true, // Ouvre automatiquement le navigateur
+    hot: true, // Active le Hot Module Replacement
+  },
   devtool: "source-map",
   mode: "development",
 };
