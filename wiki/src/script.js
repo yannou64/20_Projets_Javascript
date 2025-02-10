488badc32422bd6df4ed9acddcbf460fb177e2c2
yannick biot <yannickpro64@gmail.com>
2025-02-10 11:57:46 +0100

    je ne sais plus

--- wiki/src/script.js
+++ wiki/src/script.js
@@ -1,52 +1,55 @@
-import {img_logo} from "./logo.js";
-import {searchResults} from "./apiWiki.js"
-import {Loader} from "./loader.js"
-import {Article} from "./article.js"
+import { img_logo } from "./logo.js";
+import { searchResults } from "./apiWiki.js";
+import { Loader } from "./loader.js";
+import { Article } from "./article.js";
 
 /////////////
-//  code dynamique 
+//  code dynamique
 ////////////
 
 // cancel submit
-const form = document.querySelector("form")
+const form = document.querySelector("form");
 form.addEventListener("submit", (event) => {
-    event.preventDefault()
-})
+  event.preventDefault();
+});
 
 // "Enter" action on input
-const search = document.querySelector(".form__search")
+const search = document.querySelector(".form__search");
 
 search.addEventListener("keydown", async (event) => {
-    if (event.key === "Enter" && event.value != ""){
-        initialisationResults()
-        const isWaiting = new Loader
-        isWaiting.charge()
-        showResults(event)
-        isWaiting.uncharge()
-    }
-})
+  if (event.key === "Enter" && event.value != "") {
+    initialisationResults();
+    const isWaiting = new Loader();
+    isWaiting.charge();
+    showResults(event);
+    isWaiting.uncharge();
+  }
+});
 
 /////////////
-//  fonctions 
+//  fonctions
 ////////////
 
-async function showResults(event){
-    const results = await searchResults(event.target.value)
-    const article_element = document.querySelector(".results")
-    for (const result of results){
-        const article = new Article(result)
-        article_element.appendChild(article.article_element)
-        
-    }
+async function showResults(event) {
+  const results = await searchResults(event.target.value);
+  articlesConstruction(results);
 }
 
-function initialisationResults(){
-    const results = document.querySelector(".results")
-    results.innerHTML = ""
+function articlesConstruction(results) {
+  const articles_element = document.querySelector(".results");
+  for (const result of results) {
+    const article = new Article(result);
+    articles_element.appendChild(article.article_element);
+  }
+}
+
+function initialisationResults() {
+  const results = document.querySelector(".results");
+  results.innerHTML = "";
 }
 
 /////////////
-//  code statique 
+//  code statique
 ////////////
 const logo = document.querySelector(".logo");
 logo.appendChild(img_logo);
